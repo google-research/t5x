834d13586d4ed3557f507b2cb35f9115d1b29d86
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-07 18:45:42 +0100

    We fix tensorstore version (#15)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setuptools.setup(
         'seqio-nightly',
         't5',
         'tensorflow',
-        'tensorstore',
+        'tensorstore==0.1.13',
     ],
     extras_require={
         'gcp': [
