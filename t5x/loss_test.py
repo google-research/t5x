1cf46a6efa02a240340143fea784710ac2710150
Ryan Sepassi <rsepassi@google.com>
2021-12-09 09:16:59 -0800

    Move losses into a standalone module
    PiperOrigin-RevId: 415281589

---
+++ t5x/loss_test.py
@@ -0,0 +1,53 @@
+# Copyright 2021 The T5X Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for loss."""
+
+from absl.testing import absltest
+import jax
+import numpy as np
+from t5x import loss as loss_lib
+
+
+class LossTest(absltest.TestCase):
+
+  def test_xent(self):
+
+    def lossfn(logits, targets, weights):
+      loss, z_loss, weight_sum = loss_lib.compute_weighted_cross_entropy(
+          logits,
+          targets,
+          weights,
+          label_smoothing=0.1,
+          z_loss=0.1,
+          loss_normalizing_factor=0.1)
+      return loss, (z_loss, weight_sum)
+
+    batch_size = 2
+    length = 4
+    vocab_size = 8
+    logits = np.random.normal(size=(batch_size, length,
+                                    vocab_size)).astype(np.float32)
+    targets = np.random.randint(0, vocab_size, size=(batch_size, length))
+    weights = np.ones_like(targets)
+    out = jax.jit(jax.value_and_grad(lossfn, has_aux=True))(logits, targets,
+                                                            weights)
+    (loss, (z_loss, weight_sum)), dlogits = out
+    # Just a smoke test for now
+    # TODO(t5x): Expand test
+    print(jax.device_get(((loss, (z_loss, weight_sum)), dlogits)))
+
+
+if __name__ == '__main__':
+  absltest.main()
--- t5x/models.py
+++ t5x/models.py
@@ -23,6 +23,7 @@ import abc
 import functools
 from typing import Any, Callable, Mapping, MutableMapping, Optional, Tuple, Type, Union
 
+from absl import logging
 from flax import core as flax_core
 from flax import linen as nn
 from flax import optim
@@ -33,6 +34,7 @@ import jax.numpy as jnp
 import numpy as np
 import seqio
 from t5x import decoding
+from t5x import loss as loss_lib
 from t5x import metrics as metrics_lib
 import tensorflow as tf
 import typing_extensions
@@ -41,6 +43,27 @@ Array = Union[np.ndarray, jnp.ndarray, jax.pxla.ShardedDeviceArray, tf.Tensor]
 MetricsMap = metrics_lib.MetricsMap
 Optimizer = optim.Optimizer
 PyTreeDef = type(jax.tree_structure(None))
+
+
+# TODO(t5x): Update references and remove these.
+
+
+def _loss_move_warning(fun):
+  functools.wraps(fun)
+
+  def wrap(*args, **kwargs):
+    logging.warn('Please update your access to t5x loss functions to use '
+                 't5x.loss. The forwarding symbols in t5x.models will be '
+                 'eventually removed.')
+    return fun(*args, **kwargs)
+
+  return wrap
+
+
+cross_entropy_with_logits = _loss_move_warning(
+    loss_lib.cross_entropy_with_logits)
+compute_weighted_cross_entropy = _loss_move_warning(
+    loss_lib.compute_weighted_cross_entropy)
 
 
 class TokensIdsToLogitsCallable(typing_extensions.Protocol):
@@ -1034,140 +1057,6 @@ def remove_prefix(sequence: jnp.ndarray,
   targets = sequence * targets_and_padding_mask
   # Shift to the left by prefix length. Wrapped elements are already zeroed.
   return jnp.roll(targets, -prefix_length, axis=-1)
-
-
-@jax.custom_vjp
-def cross_entropy_with_logits(logits: jnp.ndarray, targets: jnp.ndarray,
-                              z_loss: float) -> jnp.ndarray:
-  """Computes cross entropy loss with stable custom gradient.
-
-  Computes a stabilized-gradient version of:
-    -jnp.sum(targets * nn.log_softmax(logits), axis=-1)
-
-  If z_loss > 0, then an auxiliary loss equal to z_loss*log(z)^2
-  will be added to the cross entropy loss (z = softmax normalization constant).
-  The two uses of z_loss are:
-  1. To keep the logits from drifting too far from zero, which can cause
-     unacceptable roundoff errors in bfloat16.
-  2. To encourage the logits to be normalized log-probabilities.
-
-  Args:
-    logits: [batch, length, num_classes] float array.
-    targets: categorical one-hot targets [batch, length, num_classes] float
-      array.
-    z_loss: coefficient for auxilliary z-loss loss term.
-
-  Returns:
-    tuple with the total loss and the z_loss, both
-    float arrays with shape [batch, length].
-  """
-  logits_sum = jax.scipy.special.logsumexp(logits, axis=-1, keepdims=True)
-  log_softmax = logits - logits_sum
-  loss = -jnp.sum(targets * log_softmax, axis=-1)
-  # Add auxilliary z-loss term.
-  log_z = jnp.squeeze(logits_sum, axis=-1)
-  total_z_loss = z_loss * jax.lax.square(log_z)
-  loss += total_z_loss
-  return loss, total_z_loss
-
-
-def _cross_entropy_with_logits_fwd(
-    logits: jnp.ndarray,
-    targets: jnp.ndarray,
-    z_loss: float = 0.0
-) -> Tuple[jnp.ndarray, Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp
-                              .ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]]:
-  """Forward-mode of `cross_entropy_with_logits`."""
-  max_logit = logits.max(axis=-1, keepdims=True)
-  shifted = logits - max_logit
-  exp_shifted = jnp.exp(shifted)
-  sum_exp = jnp.sum(exp_shifted, axis=-1, keepdims=True)
-  log_softmax = shifted - jnp.log(sum_exp)
-  loss = -jnp.sum(targets * log_softmax, axis=-1)
-  # Add auxilliary z-loss term.
-  log_z = jnp.squeeze(jnp.log(sum_exp) + max_logit, axis=-1)
-  total_z_loss = z_loss * jax.lax.square(log_z)
-  loss += total_z_loss
-  return (loss, total_z_loss), (logits, targets, z_loss, exp_shifted, sum_exp,
-                                log_softmax, log_z)
-
-
-def _cross_entropy_with_logits_bwd(
-    res: Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray,
-               jnp.ndarray, jnp.ndarray], g: Tuple[jnp.ndarray, jnp.ndarray]
-) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-  """Backward-mode of `cross_entropy_with_logits`."""
-  g = g[0]  # Ignore z_loss component as that is only used for logging.
-  logits, targets, z_loss, exp_shifted, sum_exp, log_softmax, log_z = res
-  # z-loss term adds the (2 * z_loss * log_z) factor.
-  deriv = (
-      jnp.expand_dims(1 + 2 * z_loss * log_z, -1) * exp_shifted / sum_exp -
-      targets)
-  g_logits = jnp.expand_dims(g, axis=-1) * deriv
-  g_targets = -jnp.expand_dims(g, axis=-1) * log_softmax
-  return (jnp.asarray(g_logits,
-                      logits.dtype), jnp.asarray(g_targets, targets.dtype),
-          jnp.array(0.0))  # sets z-loss coeff gradient to 0
-
-
-cross_entropy_with_logits.defvjp(_cross_entropy_with_logits_fwd,
-                                 _cross_entropy_with_logits_bwd)
-
-
-def compute_weighted_cross_entropy(
-    logits: jnp.ndarray,
-    targets: jnp.ndarray,
-    weights: Optional[jnp.ndarray] = None,
-    label_smoothing: float = 0.0,
-    z_loss: float = 0.0,
-    loss_normalizing_factor: Optional[float] = None
-) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-  """Compute weighted cross entropy and entropy for log probs and targets.
-
-  Args:
-   logits: [batch, length, num_classes] float array.
-   targets: categorical targets [batch, length] int array.
-   weights: None or array of shape [batch, length].
-   label_smoothing: label smoothing constant, used to determine the on and off
-     values.
-   z_loss: coefficient for auxilliary z-loss loss term.
-   loss_normalizing_factor: Constant to divide loss by. If not specified, loss
-     will not be normalized. Intended for backward compatibility with T5-MTF
-     training. Should not normally be used.
-
-  Returns:
-    Tuple of scalar loss, z_loss, and weight sum.
-  """
-  if logits.ndim != targets.ndim + 1:
-    raise ValueError('Incorrect shapes. Got shape %s logits and %s targets' %
-                     (str(logits.shape), str(targets.shape)))
-  vocab_size = logits.shape[-1]
-  confidence = 1.0 - label_smoothing
-  low_confidence = (1.0 - confidence) / (vocab_size - 1)
-  normalizing_constant = -(
-      confidence * jnp.log(confidence) +
-      (vocab_size - 1) * low_confidence * jnp.log(low_confidence + 1e-20))
-  soft_targets = common_utils.onehot(
-      targets, vocab_size, on_value=confidence, off_value=low_confidence)
-  total_loss, total_z_loss = cross_entropy_with_logits(
-      logits, soft_targets, z_loss=z_loss)
-  total_loss = total_loss - normalizing_constant
-
-  weight_sum = np.prod(targets.shape)
-  if weights is not None:
-    total_loss = total_loss * weights
-    total_z_loss = total_z_loss * weights
-    weight_sum = jnp.sum(weights)
-
-  # By default, we do not normalize loss based on anything.
-  # We don't normalize based on batch size because the optimizers we use are
-  # pretty much scale invariant, so this simplifies things.
-  # We don't normalize based on number of non-padding tokens in order to treat
-  # each token as equally important regardless of sequence length.
-  if loss_normalizing_factor:
-    total_loss /= loss_normalizing_factor
-    total_z_loss /= loss_normalizing_factor
-  return jnp.sum(total_loss), jnp.sum(total_z_loss), weight_sum
 
 
 def compute_weighted_accuracy(
