a5b976045cff855c52adcf099ca7cae531d42f37
James Lee-Thorp <jamesleethorp@google.com>
2022-03-30 17:18:31 -0700

    PjitPartitioner subclass with MoE optimizer state overrides
    PiperOrigin-RevId: 438436214

---
+++ t5x/contrib/moe/partitioning.py
@@ -0,0 +1,106 @@
+# Copyright 2022 The T5X Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Pjit partitioner with Mixture of Experts overrides."""
+
+from typing import Callable, Optional
+
+from flax import core as flax_core
+from t5x import partitioning
+from t5x import train_state as train_state_lib
+
+from t5x.contrib.moe import training_utils
+
+FlaxOptimTrainState = train_state_lib.FlaxOptimTrainState
+HardwareMesh = partitioning.HardwareMesh
+LogicalAxisRules = partitioning.LogicalAxisRules
+PartitionSpec = partitioning.PartitionSpec
+
+
+class MoePjitPartitioner(partitioning.PjitPartitioner):
+  """Pjit partitioner with overrides for Mixture of Experts support."""
+
+  def __init__(self,
+               num_partitions: Optional[int] = None,
+               model_parallel_submesh: Optional[HardwareMesh] = None,
+               params_on_devices: bool = True,
+               logical_axis_rules: Optional[LogicalAxisRules] = None,
+               state_filter_fn: Optional[Callable[[str], bool]] = None):
+    """Configures the partitioner.
+
+    Args:
+      num_partitions: Specifies the size of the model parallel submesh to be
+        automatically selected for the current topology. See
+        `model_parallel_submesh` for details on how this submesh is used.
+        Mutually exclusive with `model_parallel_submesh`.
+      model_parallel_submesh: 4-tuple that specifies the `(x, y, z, c)` submesh
+        model-parallel device tile -- an axis of accelerator parallelism
+        orthogonal to data parallelism. See t5x/partitioning.py for details.
+        This argument is mutually exclusive with `num_partitions`.
+      params_on_devices: Whether to keep the params on devices. If False, params
+        stay in the host memory.
+      logical_axis_rules: A priority-ordered sequence of KV tuples that maps
+        logical axis names to either `None` (not sharded), 'model' (to shard
+        across the model-parallel submesh), or 'data' (to shard across the
+        data-parallel submesh).
+      state_filter_fn: Function to identify which optimizer state axis rules
+        should be overridden to be sharded along the 'expert' axis. If None
+        (default), Adafactor expert sharding overrides are used.
+    """
+    super().__init__(
+        num_partitions=num_partitions,
+        model_parallel_submesh=model_parallel_submesh,
+        params_on_devices=params_on_devices,
+        logical_axis_rules=logical_axis_rules)
+    self._state_filter_fn = state_filter_fn
+
+  def get_logical_axes(self,
+                       train_state: FlaxOptimTrainState) -> FlaxOptimTrainState:
+    """Returns a copy of TrainState with Optional[AxisNames] as leaves."""
+    optimizer_axes = train_state.as_logical_axes()._optimizer  # pylint: disable=protected-access
+
+    if self._state_filter_fn:
+      state_filter_fn = self._state_filter_fn
+    else:
+      # Use default T5X Adafactor expert sharding overrides for factored
+      # kernels.
+      #
+      # The kernel terms  (`m` and `v`) that are not captured below are trivial
+      # placeholders and should not be sharded. The only nontrivial bias term
+      # (`v`) automatically inherits the correct expert partition specs through
+      # the T5X Adafactor factor rules (see derive_factor_rules():
+      # https://github.com/google-research/t5x/blob/main/t5x/adafactor.py#L591).
+      #
+      # TODO(jamesleethorp): Revisit once other T5X optimizers are available.
+      state_filter_fn = training_utils.match_fn(r'.*expert.*/kernel/v_.*')
+
+    # Prepend 'expert' axis to MoE state terms (identified by state_filter_fn)
+    # so they are sharded along the 'expert' axis.
+    prepend_expert = lambda x: PartitionSpec(  # pylint: disable=g-long-lambda
+        'expert',) + x if x else PartitionSpec('expert',)
+    state_dict = flax_core.unfreeze(optimizer_axes.state_dict())
+    state_dict['state']['param_states'] = training_utils.tree_map_with_names(
+        prepend_expert, state_dict['state']['param_states'], state_filter_fn)
+
+    return train_state.restore_state(state_dict)
+
+
+def standard_logical_axis_rules() -> partitioning.LogicalAxisRules:
+  """Returns partitioning rules for Mixture of Experts models."""
+  return (
+      ('expert', 'data'),  # Partition experts along the data axis
+      ('expert_mlp', 'model'),  # Expert MLPs partitioned along model axis
+      ('expert_group', None),  # Replicated axis for all-to-all constraints
+      ('unmodeled', None),  # Replicated weights
+  )
---
+++ t5x/contrib/moe/partitioning_test.py
@@ -0,0 +1,167 @@
+# Copyright 2022 The T5X Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for partitioning."""
+
+from typing import Any
+
+from absl.testing import absltest
+from flax import optim
+from flax.linen import partitioning as flax_partitioning
+import jax
+import numpy as np
+from t5x import train_state as train_state_lib
+
+from t5x.contrib.moe import partitioning
+from t5x.contrib.moe import training_utils
+
+AxisMetadata = flax_partitioning.AxisMetadata
+PartitionSpec = partitioning.PartitionSpec
+PRNGKey = Any
+FlaxOptimTrainState = train_state_lib.FlaxOptimTrainState
+
+
+class LogicalAdam(optim.Adam):
+  """Subclass of Adam optimizer with T5X logical axis partitioning support."""
+
+  def derive_logical_axes(self, optimizer_state, param_logical_axes):
+    """Derives optimizer logical partitioning from model logical partitions."""
+    del param_logical_axes  # Return fixed axes for test
+    optimizer_logical_axes = {
+        'state': {
+            'param_states': {
+                'logits_dense': {
+                    'grad_ema': None,
+                    'grad_sq_ema': None
+                },
+                'mlp': {
+                    'wo': {
+                        'kernel': {
+                            'grad_ema': PartitionSpec('embed', 'mlp'),
+                            'grad_sq_ema': None
+                        }
+                    }
+                }
+            },
+            'step': None
+        },
+        'target': {
+            'logits_dense': PartitionSpec('vocab', 'embed'),
+            'mlp': {
+                'wo': {
+                    'kernel': PartitionSpec('embed', 'mlp'),
+                },
+            },
+        }
+    }
+    return optimizer_state.restore_state(optimizer_logical_axes)
+
+
+def create_optimizer():
+  """Creates simple Adam optimizer."""
+  target = {
+      'logits_dense': np.ones((16, 16), np.float32),
+      'mlp': {
+          'wo': {
+              'kernel': np.ones((32, 16), np.float32)
+          }
+      }
+  }
+  return LogicalAdam(learning_rate=1e-4).create(target)
+
+
+class PartitioningTest(absltest.TestCase):
+
+  def test_logical_axes_for_moe_partitioner(self):
+    partitioner = partitioning.MoePjitPartitioner(num_partitions=1)
+
+    optimizer = create_optimizer()
+    train_state = FlaxOptimTrainState(
+        optimizer,
+        params_axes={
+            'logits_dense_axes': AxisMetadata(names=('vocab', 'embed')),
+            'mlp': {
+                'wo': {
+                    'kernel_axes': AxisMetadata(names=('embed', 'mlp'))
+                }
+            }
+        })
+
+    logical_axes = partitioner.get_logical_axes(train_state)
+
+    # No updates to state. Should match what derive_logical_axes() returns.
+    jax.tree_map(self.assertIsNone, logical_axes.param_states['logits_dense'])
+    self.assertEqual(logical_axes.param_states['mlp']['wo']['kernel'].grad_ema,
+                     PartitionSpec('embed', 'mlp'))
+    self.assertIsNone(
+        logical_axes.param_states['mlp']['wo']['kernel'].grad_sq_ema)
+
+    self.assertEqual(
+        logical_axes.params, {
+            'logits_dense': PartitionSpec('vocab', 'embed'),
+            'mlp': {
+                'wo': {
+                    'kernel': PartitionSpec('embed', 'mlp')
+                }
+            }
+        })
+
+  def test_logical_axes_for_moe_partitioner_with_overrides(self):
+    partitioner = partitioning.MoePjitPartitioner(
+        num_partitions=1, state_filter_fn=training_utils.match_fn(r'.*mlp.*'))
+
+    optimizer = create_optimizer()
+    train_state = FlaxOptimTrainState(
+        optimizer,
+        params_axes={
+            'logits_dense_axes': AxisMetadata(names=('vocab', 'embed')),
+            'mlp': {
+                'wo': {
+                    'kernel_axes': AxisMetadata(names=('embed', 'mlp'))
+                }
+            }
+        })
+
+    logical_axes = partitioner.get_logical_axes(train_state)
+
+    jax.tree_map(self.assertIsNone, logical_axes.param_states['logits_dense'])
+    # 'mlp' params should be prepended with 'expert' spec because
+    # state_filter_fn matches '.*mlp.*'.
+    self.assertEqual(logical_axes.param_states['mlp']['wo']['kernel'].grad_ema,
+                     PartitionSpec('expert', 'embed', 'mlp'))
+    self.assertEqual(
+        logical_axes.param_states['mlp']['wo']['kernel'].grad_sq_ema,
+        PartitionSpec('expert',))
+
+    self.assertEqual(
+        logical_axes.params, {
+            'logits_dense': PartitionSpec('vocab', 'embed'),
+            'mlp': {
+                'wo': {
+                    'kernel': PartitionSpec('embed', 'mlp')
+                }
+            }
+        })
+
+  def test_logical_axis_rules(self):
+    self.assertEqual(partitioning.standard_logical_axis_rules(), (
+        ('expert', 'data'),
+        ('expert_mlp', 'model'),
+        ('expert_group', None),
+        ('unmodeled', None),
+    ))
+
+
+if __name__ == '__main__':
+  absltest.main()
