5551310f76e4585386c89f51a963dc80fa44c5b9
James Lee-Thorp <jamesleethorp@google.com>
2022-04-12 08:58:13 -0700

    Package contrib.moe
    PiperOrigin-RevId: 441201765

---
+++ t5x/contrib/moe/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2022 The T5X Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Import API modules."""
+
+import t5x.contrib.moe.adafactor_utils
+import t5x.contrib.moe.models
+import t5x.contrib.moe.partitioning
+import t5x.contrib.moe.trainer
+import t5x.contrib.moe.training_utils
+
+# Version number.
+from t5x.version import __version__
