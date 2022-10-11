Make AVERAGE_PER_SEQUENCE loss normalization mode work for packed sequences. Previously it would silently fail by provided an incorrect normalization in this case.
PiperOrigin-RevId: 480347370

t5x/losses.py
@@ -204,6 +204,38 @@ def convert_special_loss_normalizing_factor_to_enum(
       'Could not convert string \"%s\" to SpecialLossNormalizingFactor' % x)
 
 
+@jax.vmap
+def _sum_weights_per_segment(positions: jnp.ndarray, segment_ids: jnp.ndarray,
+                             weights: jnp.ndarray) -> jnp.ndarray:
+  """Sums weights per packed segment to produce a normalizing vector."""
+
+  # NB: Assumes padding only occurs at the end of a sequence.
+
+  def _repeat_last_nonnegative(xs, reverse=False):
+
+    def fn(prev, x):
+      y = jnp.where(x == 0, prev, x)
+      return y, y
+
+    return jax.lax.scan(fn, jnp.zeros_like(xs[0]), xs, reverse=reverse)[1]
+
+  # Compute final positions per sequence.
+  start_positions = positions == 0
+  final_positions = jnp.concatenate([start_positions[1:], jnp.ones(1)])
+  # Clear padded positions.
+  final_positions *= segment_ids != 0
+  # Compute cumulative weights, clearing all but final position per sequence.
+  final_cumulative_weights = final_positions * jnp.cumsum(weights)
+  # Subtract sequences' final weights from cumulative weights of following ones.
+  final_total_weights = jnp.concatenate([
+      final_cumulative_weights[0:1],
+      jnp.diff(_repeat_last_nonnegative(final_cumulative_weights))
+  ])
+  # Copy final sequence weight to all positions in sequence.
+  normalizer = _repeat_last_nonnegative(final_total_weights, reverse=True)
+  return normalizer
+
+
 def get_loss_normalizing_factor_and_weights(
     loss_normalizing_factor: Optional[Union[float, int, str,
                                             SpecialLossNormalizingFactor]],
@@ -256,7 +288,15 @@ def get_loss_normalizing_factor_and_weights(
     output_normalizing_factor = np.prod(batch['decoder_target_tokens'].shape)
   elif (loss_normalizing_factor ==
         SpecialLossNormalizingFactor.AVERAGE_PER_SEQUENCE):
-    loss_weights /= jnp.sum(loss_weights, axis=-1, keepdims=True) + 1e-3
+    if 'decoder_segment_ids' in batch:  # is packed
+      norm_vec = _sum_weights_per_segment(batch['decoder_positions'],
+                                          batch['decoder_segment_ids'],
+                                          loss_weights)
+    else:
+      norm_vec = jnp.sum(loss_weights, axis=-1, keepdims=True)
+    # Handle divide-by-zero.
+    loss_weights = jnp.nan_to_num(
+        loss_weights / norm_vec, nan=0, posinf=0, neginf=0)
     output_normalizing_factor = jnp.sum(loss_weights)
   else:
     raise ValueError('Unsupported value of loss_normalizing_factor: %s' %

t5x/losses_test.py
@@ -127,9 +127,49 @@ class SpecialLossNormalizingFactorTest(absltest.TestCase):
     np.testing.assert_allclose(output_lnf, 2.0, rtol=1e-3)
     np.testing.assert_allclose(
         output_loss_weights,
+        jnp.asarray([[0.5 / 3.75, 1.0 / 3.75, 0.25 / 3.75, 2.0 / 3.75, 0.0],
+                     [1.0 / 2.0, 1.0 / 2.0, 0.0, 0.0, 0.0]], jnp.float32),
+        rtol=1e-3)
+
+  def test_sum_weights_per_segment(self):
+    weights = jnp.asarray(
+        [[0.5, 1.0, 0.25, 2.0, 1.5], [1.0, 2.0, 3.0, 4.0, 5.0]], jnp.float32)
+    positions = jnp.asarray([[0, 1, 2, 0, 0], [0, 0, 1, 0, 0]])
+    segment_ids = jnp.asarray([[1, 1, 1, 2, 3], [1, 2, 2, 3, 0]])
+
+    norm_vec = losses._sum_weights_per_segment(positions, segment_ids, weights)
+
+    np.testing.assert_allclose(
+        norm_vec,
+        jnp.asarray([[1.75, 1.75, 1.75, 2.0, 1.5], [1.0, 5.0, 5.0, 4.0, 0.0]],
+                    jnp.float32),
+        rtol=1e-3)
+
+  def test_average_per_sequence_with_weights_with_packing(self):
+    batch = {
+        'decoder_target_tokens':
+            jnp.asarray([[1, 2, 3, 4, 5], [5, 6, 7, 8, 0]], jnp.int32),
+        'decoder_loss_weights':
+            jnp.asarray([[0.5, 1.0, 0.25, 2.0, 1.5], [1.0, 2.0, 3.0, 4.0, 5.0]],
+                        jnp.float32),
+        'decoder_positions':
+            jnp.asarray([[0, 1, 2, 0, 0], [0, 0, 1, 0, 0]]),
+        'decoder_segment_ids':
+            jnp.asarray([[1, 1, 1, 2, 3], [1, 2, 2, 3, 0]])
+    }
+
+    (output_lnf,
+     output_loss_weights) = losses.get_loss_normalizing_factor_and_weights(
+         loss_normalizing_factor=losses.SpecialLossNormalizingFactor
+         .AVERAGE_PER_SEQUENCE,
+         batch=batch)
+
+    np.testing.assert_allclose(output_lnf, 6.0, rtol=1e-3)
+    np.testing.assert_allclose(
+        output_loss_weights,
         jnp.asarray(
-            [[0.1333, 0.2666, 0.0666, 0.5333, 0.0], [0.5, 0.5, 0.0, 0.0, 0.0]],
-            jnp.float32),
+            [[0.5 / 1.75, 1.0 / 1.75, 0.25 / 1.75, 2.0 / 2.0, 1.5 / 1.5],
+             [1.0 / 1.0, 2.0 / 5.0, 3.0 / 5.0, 4.0 / 4.0, 0.0]], jnp.float32),
         rtol=1e-3)
 
 if __name__ == '__main__':