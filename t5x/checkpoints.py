f95c7368cd944c5d6b1a5ea3acabdbd0f5dcad68
Colin Gaffney <cpgaffney@google.com>
2022-10-11 08:40:30 -0700

    Move DatasetCheckpointHandler out of Orbax. This can be considered a class that clients need to implement themselves, since we do not wish to keep a dependency on Tensorflow.
    PiperOrigin-RevId: 480365910

--- t5x/checkpoints.py
+++ t5x/checkpoints.py
@@ -1776,6 +1776,52 @@ class _OrbaxParamInfo:
   mesh_axes: partitioning.PartitionSpec
 
 
+class DatasetCheckpointHandler(orbax.checkpoint.CheckpointHandler):
+  """A CheckpointHandler implementation that handles tf.data.Iterator."""
+
+  def __init__(self, checkpoint_filename: str):
+    self._checkpoint_filename = checkpoint_filename
+
+  def save(self, directory: epath.Path, item: tf.data.Iterator):
+    """Saves the given item.
+
+    Args:
+      directory: save location directory.
+      item: a tf.data.Iterator to be saved.
+    """
+    if jax.process_count() > 1:
+      directory /= f'process_{jax.process_index()}-of-{jax.process_count()}'
+      directory.mkdir(parents=False, exist_ok=False)
+    ckpt = tf.train.Checkpoint(ds=item)
+    ckpt.write(os.fspath(directory / self._checkpoint_filename))
+    multihost_utils.sync_global_devices('DatasetCheckpointHandler:save')
+
+  def restore(self,
+              directory: epath.Path,
+              item: Optional[tf.data.Iterator] = None) -> tf.data.Iterator:
+    """Restores the given item.
+
+    Args:
+      directory: restore location directory.
+      item: a tf.data.Iterator to be restored. Not Optional
+
+    Returns:
+      a tf.data.Iterator restored from `directory`.
+    """
+    if item is None:
+      raise ValueError('Must provide item to restore')
+    if jax.process_count() > 1:
+      directory /= f'process_{jax.process_index()}-of-{jax.process_count()}'
+    ckpt = tf.train.Checkpoint(ds=item)
+    ckpt.read(os.fspath(directory /
+                        self._checkpoint_filename)).assert_consumed()
+    return item
+
+  def structure(self, directory: epath.Path) -> Any:
+    """Unimplemented. See parent class."""
+    return NotImplementedError
+
+
 class TrainStateCheckpointHandler(orbax.checkpoint.PyTreeCheckpointHandler):
   """Implementation of PyTreeCheckpointHandler that accounts for legacy checkpoints."""
 
@@ -1878,8 +1924,7 @@ class CheckpointManager(orbax.checkpoint.CheckpointManager):
     }
     if self._should_write_dataset_ckpt:
       checkpointers[_DATASET_KEY] = NonAtomicCheckpointer(
-          orbax.checkpoint.DatasetCheckpointHandler(
-              checkpoint_filename=dataset_ckpt_name))
+          DatasetCheckpointHandler(checkpoint_filename=dataset_ckpt_name))
 
     options = orbax.checkpoint.CheckpointManagerOptions(max_to_keep=keep)
     super().__init__(
