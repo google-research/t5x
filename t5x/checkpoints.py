79998013572b69b2234ecf43d4d09729f9e11706
Colin Gaffney <cpgaffney@google.com>
2022-12-05 11:10:50 -0800

    Fix mistaken usages of placeholder "AGGREGATED" where "NOT-AGGREGATED" would be more appropriate. Ensure backwards compatibility is maintained.
    PiperOrigin-RevId: 493059810

--- t5x/checkpoints.py
+++ t5x/checkpoints.py
@@ -1882,7 +1882,7 @@ class TrainStateCheckpointHandler(orbax.checkpoint.PyTreeCheckpointHandler):
       if isinstance(leaf, ts.Spec):
         leaf = leaf.to_json()
         leaf = epath.Path(leaf['kvstore']['path']).name
-        leaf = orbax.checkpoint.utils.aggregated_placeholder(leaf)
+        leaf = orbax.checkpoint.utils.leaf_placeholder(leaf)
       return leaf
 
     return jax.tree_map(tensorstore_spec_to_name, result)
