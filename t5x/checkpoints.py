4a7ab9ff3fa3f191bb7c95f527329c6b69e83d07
T5X Team <no-reply@google.com>
2022-12-28 09:32:19 -0800

    Internal change
    PiperOrigin-RevId: 498198594

--- t5x/checkpoints.py
+++ t5x/checkpoints.py
@@ -62,9 +62,6 @@ import tensorflow as tf
 from tensorflow.io import gfile
 import tensorstore as ts
 import typing_extensions
-from tensorboard.backend.event_processing import directory_watcher
-from tensorboard.backend.event_processing import event_file_loader
-from tensorboard.backend.event_processing import io_wrapper
 
 PartitionSpec = partitioning.PartitionSpec
 PyTreeDef = type(jax.tree_util.tree_structure(None))
