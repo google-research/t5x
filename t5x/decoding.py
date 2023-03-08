4eeebeda959e3f45447a625e59c2f8be728cdecf
Peter Hawkins <phawkins@google.com>
2023-03-08 07:47:10 -0800

    Suppress some pytype errors related to jnp.DeviceArray == jax.Array.
    PiperOrigin-RevId: 515035497

--- t5x/decoding.py
+++ t5x/decoding.py
@@ -1065,7 +1065,7 @@ def beam_search(inputs: jnp.ndarray,
 
     # If we're not at the max decode length, and the search hasn't terminated,
     # continue looping.
-    return not_at_end & (~search_terminated)
+    return not_at_end & (~search_terminated)  # pytype: disable=bad-return-type  # jax-devicearray
 
   def beam_search_loop_body_fn(state: BeamState) -> BeamState:
     """Beam search loop state update function."""
@@ -1191,7 +1191,7 @@ def beam_search(inputs: jnp.ndarray,
 
     # Update FINISHED (reached end of sentence) sequences:
     # Calculate new seq scores from log probabilities.
-    new_scores = topk_log_probs / brevity_penalty(alpha, state.cur_index + 1)
+    new_scores = topk_log_probs / brevity_penalty(alpha, state.cur_index + 1)  # pytype: disable=wrong-arg-types  # jax-devicearray
     # Mask out the still unfinished sequences by adding large negative value.
     # --> [batch, 2*beams]
     new_scores += (~newly_finished) * NEG_INF
